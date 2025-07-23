ca3864edc4a631738f880944aa1ee4584fadf9a8
NiiOscar <1NiiOscar@gmail.com>
2025-07-23 22:04:27 +0200

    Update Hero component background image to enhance visual consistency

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,7 +9,7 @@ const Hero: React.FC = () => {
     'https://plus.unsplash.com/premium_photo-1661721792327-84468396c9ef?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     'https://images.unsplash.com/photo-1627905646269-7f034dcc5738?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     'https://images.pexels.com/photos/6197119/pexels-photo-6197119.jpeg?auto=compress&cs=tinysrgb&w=1920',
-    'https://images.unsplash.com/photo-1740657254989-42fe9c3b8cce?q=80&w=3412&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    'https://plus.unsplash.com/premium_photo-1663011218145-c1d0c3ba3542?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
   ];
 
   useEffect(() => {
